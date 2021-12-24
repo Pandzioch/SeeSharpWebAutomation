8332bbcc11bff263776a73ef607ae708bc65a400
Pandzi0ch <jakub.struzik3@gmail.com>
2021-12-24 15:09:23 +0100

    Creating a template for features to test with Selenium

---
+++ SeeSharpWebAutomation/Main.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace SeeSharpWebAutomation
+{
+    [Binding]
+    public sealed class Main
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            driver.Manage().Window.FullScreen();
+            driver.Navigate().GoToUrl("http://automationpractice.com/index.php");
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            driver.Quit();
+            driver.Dispose();
+        }
+    }
+}
